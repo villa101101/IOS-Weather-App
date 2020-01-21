0273a86297a3f5467d0f3a2696a35a34f9ee460f
David <d.thomas71@yahoo.com>
2020-01-21 19:20:40 +0800

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by David Thomas on 2020/1/21.
+//  Copyright © 2020 David Thomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
